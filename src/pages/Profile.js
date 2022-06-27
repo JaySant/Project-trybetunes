65a2c5373c35b61d79415f18b095de2acc9c1747
Jay Santos <jaysant15@gmail.com>
2022-06-27 14:04:58 -0300

    Requisito 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Headers from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Profile extends React.Component {
-  render() {
-    return (
+  state = {
+    infoProfile: {},
+    loading: false,
+  };
 
+  async componentDidMount() {
+    const user = await getUser();
+    this.setState({ loading: true });
+    this.setState({ infoProfile: user, loading: false });
+  }
+
+  render() {
+    const { infoProfile, loading } = this.state;
+    console.log(infoProfile.email);
+    return (
       <>
         <Headers />
-        <div data-testid="page-profile" />
+        {loading ? <Loading /> : (
+          <div data-testid="page-profile">
+            <img
+              src={ infoProfile.image }
+              data-testid="profile-image"
+              alt="profile name"
+            />
+            <h3>Nome</h3>
+            <p>{infoProfile.name}</p>
+            <h3>Email</h3>
+            <p>{infoProfile.email}</p>
+            <h3>Description</h3>
+            <p>{infoProfile.description}</p>
+            <Link to="/profile/edit">Editar perfil</Link>
+          </div>
+        )}
       </>
     );
   }
